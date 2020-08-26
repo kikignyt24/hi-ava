54ac785a25193a2c4b2e7b9ed7d2db183c9448bc
kikiginayat24 <65651532+kikiginayat24@users.noreply.github.com>
2020-08-26 17:28:14 +0700

    ganti 2

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,15 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "avrill.v", quotes: "Neng" },
     { author: "antjedtta_", quotes: "Hehehe" },
     { author: "standinshd", quotes: "Hey! Neng Cancik! UwU" },
     { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "standinshd", quotes: "Jangan pigi dari aa yaa." },
     { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
     { author: "standinshd", quotes: "Aa minta maaf suka bikin neng kesel terus." },
 ];
--- assets/variables.js
+++ assets/variables.js
@@ -1,12 +1,6 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
   nicknames: ["Neng", "Bebeb", "Ayaaanccc", "Nengkuu", "Neneng"],
   greetings: {
-    evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
-    night: "Good Night"
     evening: "Malam Ayanncc",
     afternoon: "Sore Ayaancc",
     day: "Siang Ayaancc",
