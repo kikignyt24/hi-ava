8ebda134f86b276685be17b16f784d0aa46954dc
kikiginayat24 <65651532+kikiginayat24@users.noreply.github.com>
2020-08-26 17:27:52 +0700

    ganti

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,16 @@
 export default {
   nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Neng", "Bebeb", "Ayaaanccc", "Nengkuu", "Neneng"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
     day: "Good Day",
     morning: "Good Morning",
     night: "Good Night"
+    evening: "Malam Ayanncc",
+    afternoon: "Sore Ayaancc",
+    day: "Siang Ayaancc",
+    morning: "Morn Ayaaancc",
+    night: "Met Bobo Ayaancc"
   }
 };
